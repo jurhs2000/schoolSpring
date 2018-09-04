d00e99e2cf2eb51a5cd6d6f776f8b4c9bcb83c46
jurhs2000 <jurhs2000@gmail.com>
2018-09-04 08:26:51 -0600

    modificado - OnlineCourseController, falataba metodo `post`

--- src/main/java/com/julioherrera/school/core/controller/OnlineCourseController.java
+++ src/main/java/com/julioherrera/school/core/controller/OnlineCourseController.java
@@ -1,6 +1,7 @@
 package com.julioherrera.school.core.controller;
 
 import com.julioherrera.school.core.bs.dao.CourseRepository;
+import com.julioherrera.school.core.bs.dao.OnlineCourseRepository;
 import com.julioherrera.school.core.eis.bo.Course;
 import com.julioherrera.school.core.eis.bo.OnlineCourse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,18 +13,18 @@ import org.springframework.web.bind.annotation.*;
 @ResponseBody
 public class OnlineCourseController {
     @Autowired
-    private CourseRepository courseRepository;
+    private OnlineCourseRepository onlineCourseRepository;
     @RequestMapping(method = RequestMethod.GET)
-    public Iterable<Course> getAll() {
-        return courseRepository.findAll();
+    public Iterable<OnlineCourse> getAll() {
+        return onlineCourseRepository.findAll();
     }
     @RequestMapping(value = "{id}", method = RequestMethod.PATCH)
     public Object save(@PathVariable("id") Long id, @RequestBody(required = true) OnlineCourse onlineCourse) {
         onlineCourse.setCourseId(id);
-        return courseRepository.save(onlineCourse);
+        return onlineCourseRepository.save(onlineCourse);
     }
     @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
     public void delete(@PathVariable("id") Long id) {
-        courseRepository.deleteById(id);
+        onlineCourseRepository.deleteById(id);
     }
 }
